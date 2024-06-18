d508042bb501085733117145b017c8b9273d93c5
Susanna Kayed <kefka.sk@gmail.com>
2024-06-18 10:40:56 +0200

    created class datamanager

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,22 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        load_dotenv(dotenv_path=".env")
+        self.s_user = os.environ.get('username')
+        self.s_project = os.environ.get('projectName')
+        self.s_name = os.environ.get('sheetName')
+        self.s_endpoint = f'https://api.sheety.co/{self.s_user}/{self.s_project}/{self.s_name}'
+
+        
+
+    def read_file(self):
+        s_response = requests.get(self.s_endpoint)
+        s_response.raise_for_status
+
+        return s_response.json()['prices']
--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.  
-import requests
-import os
-from dotenv import load_dotenv
+from data_manager import DataManager
 from pprint import pprint
 
-load_dotenv(dotenv_path=".env")
-# sheety credentials
+sheet = DataManager()
+sheet_data = sheet.read_file() ## dictionaries list
 
-s_user = os.environ.get('username')
-s_project = os.environ.get('projectName')
-s_name = os.environ.get('sheetName')
-
-s_endpoint = f'https://api.sheety.co/{s_user}/{s_project}/{s_name}'
-
-s_response = requests.get(s_endpoint)
-s_response.raise_for_status
-
-pprint(s_response.json())
+pprint(sheet_data)
