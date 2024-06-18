5362a966116d62cd02883c102dee1505b93db70f
Susanna Kayed <kefka.sk@gmail.com>
2024-06-18 11:05:41 +0200

    check if iata empty and update it

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.  
 from data_manager import DataManager
+from flight_search import FlightSearch
 from pprint import pprint
 
 sheet = DataManager()
 sheet_data = sheet.read_file() ## dictionaries list
 
-pprint(sheet_data)
+#{'city': 'Paris', 'iataCode': '', 'id': 2, 'lowestPrice': 54}
+
+for city in sheet_data:
+    if city['iataCode'] == '':
+        updateIata = FlightSearch()
+        city['iataCode'] = updateIata.changeIata(city)
+        sheet.update_file(city)
+        
+
